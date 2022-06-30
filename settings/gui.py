4f8d68a5e9a5aaea2dcf5bfcfdecfdcd18724edf
Jurica Barta <juricabarta@gmail.com>
2022-06-30 21:26:55 +0200

    Color button change

--- settings/gui.py
+++ settings/gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import Button, ttk
+from tkinter.colorchooser import askcolor
 
 class SettingsModule(tk.Frame):
     def __init__(self, master, *args, **kwargs):
@@ -10,3 +12,14 @@ class SettingsModule(tk.Frame):
     def create_test_label(self):
         self.test_label = tk.Label(self, text="Settings", font=self.font)
         self.test_label.grid(row=0, column=0)
+
+    def change_color(self):
+        colors = askcolor(title="Tkinter Color Chooser")
+        self.configure(bg=colors[1])
+        
+
+    def color_button(self):
+        self.button = ttk.Button(
+        text='Select a Color',
+        command=self.change_color).pack(expand=True)
+        self.test_label.grid(row=1, column=1)    
